c6604bccb27cdb234c15dbe8247977fe80a0c67b
tomyork817 <368050@edu.itmo.ru>
2024-08-18 01:29:48 +0500

    use cfg in gmail constructor

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,7 +1,14 @@
 package app
 
-import "route256-gmail-checker/pkg/config"
+import (
+	"route256-gmail-checker/pkg/config"
+)
 
 func Run(cfg *config.Config) {
-
+	//ctx := context.Background()
+	//
+	//gmailService, err := gmail.NewGmailService(ctx, cfg.GoogleAPI)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 }
--- pkg/gmail/gmail.go
+++ pkg/gmail/gmail.go
@@ -13,31 +13,23 @@ import (
 	"os"
 )
 
-const (
-	credentialsFile = "credentials.json"
-	tokenFile       = "token.json"
-)
-
 type Config struct {
 	Directory       string `mapstructure:"directory" validate:"required"`
 	CredentialsFile string `mapstructure:"credentials_file" validate:"required"`
 	TokenFile       string `mapstructure:"token_file" validate:"required"`
 }
 
-func NewGmailService(ctx context.Context, gmailScope string) (*gmail.Service, error) {
-	b, err := os.ReadFile(credentialsFile)
+func NewGmailService(ctx context.Context, cfg Config) (*gmail.Service, error) {
+	b, err := os.ReadFile(fmt.Sprintf("%s/%s", cfg.Directory, cfg.CredentialsFile))
 	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
-		return nil, err
+		return nil, fmt.Errorf("unable to read client secret file: %w", err)
 	}
 
-	// If modifying these scopes, delete your previously saved token.json.
-	config, err := google.ConfigFromJSON(b, gmailScope)
+	config, err := google.ConfigFromJSON(b, gmail.GmailReadonlyScope)
 	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
-		return nil, err
+		return nil, fmt.Errorf("unable to parse client secret file to config: %w", err)
 	}
-	client, err := getClient(config)
+	client, err := getClient(config, fmt.Sprintf("%s/%s", cfg.Directory, cfg.TokenFile))
 	if err != nil {
 		return nil, err
 	}
@@ -51,12 +43,7 @@ func NewGmailService(ctx context.Context, gmailScope string) (*gmail.Service, er
 	return srv, nil
 }
 
-// Retrieve a token, saves the token, then returns the generated client.
-func getClient(config *oauth2.Config) (*http.Client, error) {
-	// The file token.json stores the user's access and refresh tokens, and is
-	// created automatically when the authorization flow completes for the first
-	// time.
-
+func getClient(config *oauth2.Config, tokenFile string) (*http.Client, error) {
 	tok, err := tokenFromFile(tokenFile)
 	if err != nil {
 		tok, err = getTokenFromWeb(config)
@@ -71,7 +58,6 @@ func getClient(config *oauth2.Config) (*http.Client, error) {
 	return config.Client(context.Background(), tok), nil
 }
 
-// Request a token from the web, then returns the retrieved token.
 func getTokenFromWeb(config *oauth2.Config) (*oauth2.Token, error) {
 	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
 	fmt.Printf("Go to the following link in your browser then type the "+
@@ -79,19 +65,16 @@ func getTokenFromWeb(config *oauth2.Config) (*oauth2.Token, error) {
 
 	var authCode string
 	if _, err := fmt.Scan(&authCode); err != nil {
-		log.Fatalf("Unable to read authorization code: %v", err)
-		return nil, err
+		return nil, fmt.Errorf("unable to read authorization code: %w", err)
 	}
 
 	tok, err := config.Exchange(context.TODO(), authCode)
 	if err != nil {
-		log.Fatalf("Unable to retrieve token from web: %v", err)
-		return nil, err
+		return nil, fmt.Errorf("unable to retrieve token from web: %w", err)
 	}
 	return tok, nil
 }
 
-// Retrieves a token from a local file.
 func tokenFromFile(file string) (*oauth2.Token, error) {
 	f, err := os.Open(file)
 	if err != nil {
@@ -103,15 +86,15 @@ func tokenFromFile(file string) (*oauth2.Token, error) {
 	return tok, err
 }
 
-// Saves a token to a file path.
 func saveToken(path string, token *oauth2.Token) error {
 	fmt.Printf("Saving credential file to: %s\n", path)
 	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
-		return err
+		return fmt.Errorf("unable to cache oauth token: %w", err)
 	}
 	defer f.Close()
-	json.NewEncoder(f).Encode(token)
+	if err = json.NewEncoder(f).Encode(token); err != nil {
+		return err
+	}
 	return nil
 }
