ebb50c7bcca5b608a9e75d59d12699399e4c42ef
tomyork817 <368050@edu.itmo.ru>
2024-08-18 03:06:14 +0500

    fix name of interface to mail

--- internal/usecase/checker/interfaces.go
+++ internal/usecase/checker/interfaces.go
@@ -2,7 +2,7 @@ package checker
 
 import "route256-gmail-checker/internal/domain"
 
-type Gmail interface {
+type Mail interface {
 	GetLast10MessageIDs(searchQuery string) ([domain.MessagesListLen]string, error)
 	GetMessageByID(id string) (domain.Message, error)
 	DeleteLabelByID(messageID, labelID string) error
